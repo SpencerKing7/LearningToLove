1e61a20ebe4f66e59e8c7337d1c823bcda095709
SpencerKing7 <spencerking.c@gmail.com>
2025-12-02 10:40:03 -0600

    Deploy updates

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import './styles/custom.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/LearningToLove">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
