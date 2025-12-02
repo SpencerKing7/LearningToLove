8496860212cb52067409e9f0c77636308d298df6
SpencerKing7 <spencerking.c@gmail.com>
2025-12-02 11:37:37 -0600

    Updated components: Navbar,custom. Deployed to GitHub Pages

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,19 @@
 import { Link } from 'react-router-dom'
 
 function Navbar() {
+  const closeNavbar = () => {
+    const navbarCollapse = document.getElementById('navbarSupportedContent')
+    const navbarToggler = document.querySelector('.navbar-toggler') as HTMLButtonElement
+
+    if (navbarCollapse?.classList.contains('show')) {
+      navbarToggler?.click()
+    }
+  }
+
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top" id="top">
       <div className="container">
-        <Link className="navbar-brand" to="/">Home</Link>
+        <Link className="navbar-brand" to="/" onClick={closeNavbar}>Home</Link>
         <button
           className="navbar-toggler"
           type="button"
@@ -19,13 +28,13 @@ function Navbar() {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link className="nav-link" to="/story">My Story</Link>
+              <Link className="nav-link" to="/story" onClick={closeNavbar}>My Story</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/questions">Q&A</Link>
+              <Link className="nav-link" to="/questions" onClick={closeNavbar}>Q&A</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/learnmore">Learn More</Link>
+              <Link className="nav-link" to="/learnmore" onClick={closeNavbar}>Learn More</Link>
             </li>
           </ul>
         </div>
